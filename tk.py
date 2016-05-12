5fec6791e6492cb1c181e4f866e1e0fb45e7ab29
scturtle <scturtle@gmail.com>
2016-05-12 11:04:16 +0800

    change algorithm to compute tk

--- tk.py
+++ tk.py
@@ -1,6 +1,18 @@
+import re
 import sys
-import time
 import ctypes
+try:
+    import urllib2 as request
+except:
+    from urllib import request
+
+
+def get_d1():
+    req = request.Request(url='http://translate.google.com/', headers={'User-Agent': 'Mozilla/5.0'})
+    t = request.urlopen(req).read().decode('utf8')
+    a, b, h = re.search(r"TKK=eval\(\'\(\(function\(\){var a\\x3d(\-?\d+);var b\\x3d(\-?\d+);return (\d+)", t).groups()
+    return int(h), ctypes.c_int32(int(a) + int(b)).value
+b, d1 = get_d1()
 
 
 def RL(a, b):
@@ -14,7 +26,6 @@ def RL(a, b):
 
 
 def calc_tk(a):
-    b = int(time.time() / 3600)
     if sys.version_info >= (3,):
         d = a.encode('utf-8')
     else:
@@ -23,6 +34,7 @@ def calc_tk(a):
     for di in d:
         a = RL(a + di, "+-a^+6")
     a = RL(a, "+-3^+b+-f")
+    a = ctypes.c_int32(a ^ d1).value
     a = a if a >= 0 else ((a & 2147483647) + 2147483648)
     a %= pow(10, 6)
     return '%d.%d' % (a, a ^ b)
